cfddffa732c687661a5306f0527a41ce44245ca6
snehamoybag <snehamoybag2@gmail.com>
2023-04-20 21:30:39 +0530

    display ip infos on DOM

--- app.js
+++ app.js
@@ -11,24 +11,31 @@ const getUserTypedIp = () => { };
 const getUserIp = async () => {
     const response = await fetch('https://api.ipify.org?format=json');
     const data = await response.json();
-    //console.log(response)
     return data.ip;
 };
 
-const getIpGeodata = async (ip) => {
+const getIpGeoData = async (ip) => {
     const resolvedIp = await ip;
     const url = `https://geo.ipify.org/api/v2/country,city?apiKey=at_Kc5uBj4UW4W2O8VW74w4z2mCytjRC&ipAddress=${resolvedIp}`;
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
     return data;
 };
 
-// show user info on page load
-getIpGeodata(getUserIp());
+const displayIpGeoData = async (ip) => {
+    const displayIpAddress = document.querySelector('#user-ip-address');
+    const displayLocation = document.querySelector('#user-location');
+    const displayTimeZone = document.querySelector('#user-time-zone');
+    const displayISP = document.querySelector('#user-isp');
+    // get ip data
+    const data = await getIpGeoData(ip);
+    console.log(data);
+    // display data
+    displayIpAddress.textContent = data.ip;
+    displayLocation.textContent = `${data.location.region}, ${data.location.city} ${data.location.postalCode}`;
+    displayTimeZone.textContent = `UTC ${data.location.timezone}`;
+    displayISP.textContent = data.isp;
+};
 
-// show user typed ip info on click
-document.querySelector('#form-get-ip').addEventListener('submit', (e) => {
-    e.preventDefault();
-    getIpGeodata(getUserTypedIp())
-});
+// display ip info on page load
+displayIpGeoData(getUserIp());
